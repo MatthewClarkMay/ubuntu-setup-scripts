804b4be0069437079e537dc31ace2f27e2e3c462
MatthewClarkMay <mcmay.web@gmail.com>
2016-02-04 14:15:47 -0500

    Tweaked stuff

--- ubuntu-fresh-setup.sh
+++ ubuntu-fresh-setup.sh
@@ -47,7 +47,7 @@ echo "-------------------------------------------------------------------"
 echo "-------------------------- Next Phase -----------------------------"
 echo "-------------------------------------------------------------------"
 
-sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-wallpapers* htop git hexedit exiftool exif nmap vim volatility qbittorrent chromium-browser gufw speedcrunch gparted -y
+sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-wallpapers* htop hexedit exiftool exif nmap vim volatility qbittorrent chromium-browser gufw speedcrunch gparted -y
 
 echo "-------------------------------------------------------------------"
 echo "-------------------------- Next Phase -----------------------------"
@@ -108,7 +108,7 @@ echo "Cleaning Up" &&
     sudo apt-get -y clean
 
 echo "-------------------------------------------------------------------"
-echo "-------------------------- Next Phase -----------------------------"
+echo "------------------------- Shutting Down ---------------------------"
 echo "-------------------------------------------------------------------"
 
 sudo reboot
