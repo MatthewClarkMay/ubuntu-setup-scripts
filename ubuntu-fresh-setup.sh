ad3f6201148c6a269aa6003aca8cd867c0501993
MatthewClarkMay <mcmay.web@gmail.com>
2016-02-04 00:19:39 -0500

    Added 'Next Phase' delimiter in script added 'q' alias

--- ubuntu-fresh-setup.sh
+++ ubuntu-fresh-setup.sh
@@ -2,6 +2,10 @@
 
 sudo sed -i "/^# deb .*partner/ s/^# //" /etc/apt/sources.list
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 echo "Downloading GetDeb and PlayDeb" &&
     wget http://archive.getdeb.net/install_deb/getdeb-repository_0.1-1~getdeb1_all.deb http://archive.getdeb.net/install_deb/playdeb_0.3-1~getdeb1_all.deb &&
     echo "Installing GetDeb" &&
@@ -12,6 +16,10 @@ echo "Downloading GetDeb and PlayDeb" &&
     rm -f getdeb-repository_0.1-1~getdeb1_all.deb &&
     rm -f playdeb_0.3-1~getdeb1_all.deb
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 sudo add-apt-repository -y ppa:videolan/stable-daily
 sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 sudo add-apt-repository -y ppa:gnome3-team/gnome3
@@ -19,15 +27,31 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:webupd8team/y-ppa-manager
 sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 echo 'deb http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
     echo 'deb-src http://download.videolan.org/pub/debian/stable/ /' | sudo tee -a /etc/apt/sources.list.d/libdvdcss.list &&
     wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc|sudo apt-key add -
+
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
 
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
-sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-wallpapers* htop git hexedit exiftool exif nmap vim volatility qbittorrent chromium-browser gufw speedcrunch gparted
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
+sudo apt-get install synaptic vlc gimp gimp-data gimp-plugin-registry gimp-data-extras y-ppa-manager bleachbit openjdk-7-jre oracle-java8-installer unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller libxine1-ffmpeg mencoder flac faac faad sox ffmpeg2theora libmpeg2-4 uudeview libmpeg3-1 mpeg3-utils mpegdemux liba52-dev mpeg2dec vorbis-tools id3v2 mpg321 mpg123 libflac++6 totem-mozilla icedax lame libmad0 libjpeg-progs libdvdcss2 libdvdread4 libdvdnav4 libswscale-extra-2 ubuntu-wallpapers* htop git hexedit exiftool exif nmap vim volatility qbittorrent chromium-browser gufw speedcrunch gparted -y
+
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
 
 sudo apt-get install wireshark
 sudo dpkg-reconfigure wireshark-common
@@ -48,24 +72,43 @@ else
     rm -f google-chrome-stable_current_i386.deb
 fi
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 sudo mv /etc/vim/vimrc /etc/vim/vimrc-backup
 sudo mv vimrc /etc/vim/
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 mkdir ~/.vim/colors
 wget https://raw.githubusercontent.com/DrSpatula/vim-buddy/master/colors/buddy.vim
 mv buddy.vim ~/.vim/colors
 
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
+
 sudo mv ~/.bashrc ~/.bashrc-copy
 sudo mv .bashrc ~
-
 sudo mv .bash_aliases ~
+
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
 
 echo "Cleaning Up" &&
     sudo apt-get -f install &&
     sudo apt-get autoremove &&
     sudo apt-get -y autoclean &&
     sudo apt-get -y clean
+
+echo "-------------------------------------------------------------------"
+echo "-------------------------- Next Phase -----------------------------"
+echo "-------------------------------------------------------------------"
 
 sudo reboot
